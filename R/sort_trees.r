3993d6b87ad32a4da4475359344564e280f3d8ef
Luiz Max F. Carvalho <lmcarvalho@ufrj.br>
2015-02-09 12:27:46 +0000

    fixed a bug with tip.labels

--- R/sort_trees.r
+++ R/sort_trees.r
@@ -1,7 +1,7 @@
 # Takes a set of trees and calculates the weights. Then sorts them and  adds trees to 'sorted' until the sum of weights  <= alpha
 # TODO: find if we should also put a limit on the number of trees
 sort_trees <- function(trees, N = 4096, alpha = .95){ 
- posts <- abs( unlist(lapply(trees, function(tree) tree$posterior)) ) # inverse of the posterior to get the weights  right
+ posts <- abs (unlist(lapply(trees, function(tree) tree$posterior)) )
  weights <- posts/sum(posts) # normalised weights 
  sweights <- sort(weights)
  pos <- sapply(sweights, function(x) match(x, weights)) 
@@ -10,10 +10,10 @@ sort_trees <- function(trees, N = 4096, alpha = .95){
  class(sorted) <- class(trees)
  i <- 1 
  sum <- 0
- #
- while(sum <= alpha  && i <= N){
+ # 
+ while(sum <= alpha && i <= N){
    sum <- sum + as.numeric(sweights[i])
-   sorted[i] <- trees[pos[i]]
+   sorted[[i]] <- trees[[pos[i]]]
      i <- i + 1   
  }
  cat("Total mass: ", sum, "\n")
