2a5c1328bc294fcd992609c6b342744fb62b973b
Luiz Max F. Carvalho <lmcarvalho@ufrj.br>
2015-02-09 12:22:48 +0000

    Adding extra N restriction

--- R/sort_trees.r
+++ R/sort_trees.r
@@ -10,13 +10,12 @@ sort_trees <- function(trees, N = 4096, alpha = .95){
  class(sorted) <- class(trees)
  i <- 1 
  sum <- 0
- # && i <= N
- while(sum <= alpha){
+ #
+ while(sum <= alpha  && i <= N){
    sum <- sum + as.numeric(sweights[i])
    sorted[i] <- trees[pos[i]]
      i <- i + 1   
  }
-#  cat("Total mass: ", sum, "\n")
-#   if(length(sorted)>N){sorted <- sorted[1:N]} # if we put the limit of at most N trees 
+ cat("Total mass: ", sum, "\n")
  return(sorted)
 }
