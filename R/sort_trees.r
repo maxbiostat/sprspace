61502d00a2efb60bd8e7dd38ec3c9a0db5490c58
Luiz Max de Carvalho <lmcarvalho@ufrj.br>
2015-03-18 18:14:00 +0000

    much faster (orders of magnitude) implementation

--- R/sort_trees.r
+++ R/sort_trees.r
@@ -1,21 +1,33 @@
 # Takes a set of unique trees and calculates the weights. Then sorts them and  adds trees to 'sorted' until the sum of weights  <= alpha
-sort_trees <- function(trees, N = 4096, alpha = .95){ 
- hits <- as.vector(trees[[2]]) # "hit" information from unique_trees()
- trees <- trees[[1]]
- weights <- hits/sum(hits) # normalised weights 
- sweights <- sort(weights, decreasing = TRUE)
- pos <- sapply(sweights, function(x) match(x, weights)) 
-   #
- sorted <- list() # hate objects of varying size, but...
- class(sorted) <- class(trees)
- i <- 1 
- sum <- 0
- # 
- while(sum <= alpha && i <= N){
-   sum <- sum + as.numeric(sweights[i])
-   sorted[[i]] <- trees[[pos[i]]]
-     i <- i + 1   
- }
- cat("Total mass: ", sum, "\n")
- return(sorted)
+sort_trees <- function(utrees, N = 4096, alpha = .95){ 
+  utrees.trees <- utrees[[1]]
+  hits <- as.vector(utrees[[2]]) # "hit" information from unique_trees()
+  s.hits <- sort(hits, decreasing = TRUE)
+  sweights <- s.hits/sum(s.hits)
+  n <- length(hits)
+  #  tab <- data.frame( name = names(utrees.trees), freq = hits)
+  pos <- rep(NA, n)
+  for(j in 1:n){
+    all.ps <- grep(s.hits[j], hits)
+    u.ps <- setdiff(all.ps, na.omit(pos))
+    if(length(u.ps)==1){
+      pos[j] <- u.ps 
+    } else{
+      random.p <- sample(u.ps, 1) # breaking ties randomly
+      pos[j] <- random.p
+      
+    }  
+  }
+  sorted <- list() # hate objects of varying size, but...
+  class(sorted) <- class(utrees.trees)  
+  i <- 1 
+  sum <- 0
+  while(sum <= alpha && i <= N){
+    sum <- sum + as.numeric(sweights[i])
+    sorted[[i]] <- utrees.trees[[pos[i]]]
+    i <- i + 1   
+  }
+  cat("Total mass: ", sum, "\n")
+  names(sorted) <- names(utrees.trees)[pos[1:(i-1)]]
+  return(sorted)
 }
--- R/unique_trees.r
+++ R/unique_trees.r
@@ -1,29 +1,50 @@
-# Slight modification of the orignal unique.multiPhylo() to keep track of how many times it has seen a particular topology
-unique_trees <- function (x, incomparables = FALSE, use.edge.length = FALSE, 
-          use.tip.label = TRUE, ...) 
-{
-  n <- length(x)
-  keep <- 1L
-  old.index <- seq_len(n)
-  hits <- rep(1, n)
-  for (i in 2:n) {
-    cat("Doing tree", i, "\n")
-    already.seen <- FALSE
-    for (j in keep) {
-      if (all.equal(x[[j]], x[[i]], use.edge.length = use.edge.length, 
-                    use.tip.label = use.tip.label)) {
-        already.seen <- TRUE
-        old.index[i] <- j
-        hits[j] <- hits[j] + 1
-        break
-      }
-    }
-    if (!already.seen) 
-      keep <- c(keep, i)
+library(ape)
+library(phybase)
+#################
+nw.order <- function(tree){
+  system(input = write.tree(tree), command = "nw_order -", intern = TRUE)
+}
+# nw.order(rmtree(n = 50, N = 100))
+obtain.urep <- function(trees){
+  raw.trees <- nw.order(trees)
+  n <- length(raw.trees)
+  utrees <- vector(n, mode = "list")
+  for (i in 1:n) {utrees[[i]] <- read.tree(text = raw.trees[i])}
+  class(utrees) <- "multiPhylo"
+  return(utrees)
+}
+
+tree2string <- function(tree) {nw <- write.tree(tree); return( gsub(":[0-9]+", "", gsub("\\.", "", nw)))} 
+unique_trees <- function(trees){
+  trees <- obtain.urep(trees)
+  tt <- table(as.character(lapply(trees, tree2string)))
+  n <- length(tt)
+  new.trees <- vector(n, mode = "list")
+  for(i in 1:n){
+    new.trees[[i]] <- read.tree(text = names(tt)[i])
   }
-  hits <- hits[keep]
-  res <- x[keep]
-  res <- list(res, hits)
-  attr(res, "old.index") <- old.index
+  class(new.trees) <- "multiPhylo"
+  res <- list(trees = new.trees,  hits  = as.numeric(tt))
   res
 }
+#################
+## Testing
+# utrees <- rmtree(n = 17, N = 1500, rooted = TRUE)
+# M <- 10000
+# fake.posterior <- vector(M, mode = "list")
+# class(fake.posterior) <- class(utrees)
+# for(i in 1:M){
+#   fake.posterior[[i]] <- utrees[[sample(1:1500, 1)]]  
+# }
+# write.tree(fake.posterior, file = "~/test_posterior_1494.nwk")
+# system.time(
+#   Utrees <- unique_trees(fake.posterior)
+#   )
+# 
+# ## real test
+# source("../../GREPO/R/read.nexusB.r")
+# real.posterior <- read.nexusB("../../DATA/dengue/Dengue4.env.trees")
+# system.time(
+#   uposterior <- unique_trees(real.posterior)
+#   )
+# max(uposterior[[2]])
