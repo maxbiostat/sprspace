7eeabc0fce7fd0bcb920fcbfdfd12da953da3d7c
Luiz Max F. Carvalho <lmcarvalho@ufrj.br>
2015-03-19 09:44:57 +0000

    Update spRspace_MWE_Denv4.R

--- R/spRspace_MWE_Denv4.R
+++ R/spRspace_MWE_Denv4.R
@@ -6,7 +6,6 @@ library(devtools)
 source("read.nexusB.r")
 source("unique_trees.r")
 source("sort_trees.r")
-source("compute_topological_matrix.R")
 source_url("https://raw.githubusercontent.com/maxbiostat/rspr/master/R/rspR.r")
 source("spRspace_aux.R")
 ##
